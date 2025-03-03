7a7bb43c40f325b0d2beb6d6cd6eedb3e575f74a
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-03-02 23:32:52 -0600

    Format

--- src/components/utils/posts.data.ts
+++ src/components/utils/posts.data.ts
@@ -10,7 +10,7 @@ export type PostData = {
     date: Date;
 };
 
-function get_post_data(file: string): PostData & {draft: boolean} {
+function get_post_data(file: string): PostData & { draft: boolean } {
     const contents = fs.readFileSync(`src/posts/${file}`, "utf-8");
     const frontmatter = fm<Record<string, string | Date | number | boolean>>(contents);
     return {
@@ -27,14 +27,16 @@ export { data };
 function load(): PostData[] {
     return fs
         .readdirSync("src/posts/")
-        .filter(file => file.endsWith(".md") && file !== "index.md" && (process.env.MODE !== "prod" || file !== "test.md"))
+        .filter(
+            file => file.endsWith(".md") && file !== "index.md" && (process.env.MODE !== "prod" || file !== "test.md"),
+        )
         .map(file => {
             console.log(file);
             return file;
         })
         .map(get_post_data)
         .filter(post => !post.draft)
-        .map(({path, title, date}) => ({path, title, date}))
+        .map(({ path, title, date }) => ({ path, title, date }))
         .sort((a, b) => b.date.getTime() - a.date.getTime());
 }
 
