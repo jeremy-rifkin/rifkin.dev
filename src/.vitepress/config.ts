a272370cb34a8dc925c37efaafc22b6b03154e3b
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2025-03-05 22:57:01 -0600

    Add llvm ir as a language for syntax highlighting

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from "vitepress";
 import footnote from "markdown-it-footnote";
+import { Highlighter } from "shiki";
+import yaml from "yaml";
+import fs from "fs";
+
+const llvm_textmate = yaml.parse(fs.readFileSync("vendor/ll.tmLanguage.yaml", "utf-8"));
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -13,6 +18,9 @@ export default defineConfig({
         toc: { level: [1, 2, 3] },
         config: md => {
             md.use(footnote);
+        },
+        shikiSetup: async (shiki: Highlighter) => {
+            await shiki.loadLanguage(llvm_textmate);
         },
     },
     cleanUrls: true,
